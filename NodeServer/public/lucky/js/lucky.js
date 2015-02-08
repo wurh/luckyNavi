b482ad7a5884635da796aa0975940d621d13a772
吴荣华 <wurh@ucweb.com>
2015-02-08 09:21:48 +0800

    [UPD] 完成选项处理

--- NodeServer/public/lucky/js/lucky.js
+++ NodeServer/public/lucky/js/lucky.js
@@ -1,6 +1,7 @@
-(function(){
-'use strict';
+(function () {
+  'use strict';
   var lucky;
+  var luckyTime = 1; //中奖个数
   $(document).ready(function () {
     lucky = new Lucky();
     bindEvent();
@@ -10,39 +11,55 @@
   /**
    * 绑定按钮事件
    */
-  function bindEvent(){
+  function bindEvent() {
 
-      $('#st').bind('click',function(){
-        $('#st').addClass('indianred');
-        $('#cl').removeClass('indianred');
-        lucky.roll();
-      });
+    //开始按钮事件
+    $('#st').bind('click', function () {
+      $('#st').addClass('indianred');
+      $('#cl').removeClass('indianred');
+      lucky.roll();
+    });
 
-    $('#cl').bind('click',function(){
+    //结束按钮事件
+    $('#cl').bind('click', function () {
       $('#cl').addClass('indianred');
       $('#st').removeClass('indianred');
       lucky.stopRoll();
     });
+
+    //选择中奖数按钮事件
+    $('#itmbtn').bind('click', function () {
+      $('#itm-balls').show();
+    });
+
+    var balls = $('.bls-btn');
+    for (var i = 0; i < balls.length; i++) {
+      $(balls[i]).bind('click', function (e) {
+        $('#itmbtn').html(e.currentTarget.innerHTML);
+        $('#itm-balls').hide();
+      })
+      //console.log(balls[i]);
+    }
   }
 
-  function Lucky(){
-    this.oriArr = ['张三','李四','王五','陈六','李七','吴八']
+  function Lucky() {
+    this.oriArr = ['张三', '李四', '王五', '陈六', '李七', '吴八']
   }
 
   /**
    * 开始抽奖的方法
    */
-  Lucky.prototype.roll = function(){
+  Lucky.prototype.roll = function () {
     var _me = this;
-    _me.timer = setInterval(function(){
-      var i = parseInt(Math.random()*(_me.oriArr.length-1))
+    _me.timer = setInterval(function () {
+      var i = parseInt(Math.random() * (_me.oriArr.length - 1))
       $('#lktxt').text(_me.oriArr[i]);
-    },100);
+    }, 100);
   }
 
-  Lucky.prototype.stopRoll = function(){
+  Lucky.prototype.stopRoll = function () {
     var _me = this;
-    if(_me && _me.timer){
+    if (_me && _me.timer) {
       clearInterval(_me.timer);
     }
   }
